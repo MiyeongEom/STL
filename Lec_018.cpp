8c9dad43dddf653bc4a88aceef2fa0e2b16be93c
MiyeongEom <aldjd020420@naver.com>
2023-04-25 02:55:41 +0900

    array - 정렬

---
+++ Lec_018.cpp
@@ -0,0 +1,32 @@
+﻿// STL 3월 28일 강의기록
+
+// array - 정렬(ranges::sort)
+
+#include <iostream>
+#include <array>
+#include <ranges>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	array < string, 5> a{
+		"서울대에"s, "정순신"s, "민사고"s, "동창"s, "대자보"s
+	};
+
+
+	// 오름차순 정렬해서 출력해라
+	/*
+	sort(a.begin(), a.end(), [](string a, string b) {
+		return a < b;
+		});
+
+	for (string str : a)
+		cout << str << endl;
+	*/
+
+	ranges::sort(a);
+
+	for (const string& str : a)
+		cout << str << endl;
+}

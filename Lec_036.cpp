406803edd850c67c3ac850b9c59dbeb41a3fa391
MiyeongEom <aldjd020420@naver.com>
2023-04-25 16:23:04 +0900

    Deque - 중복제거

---
+++ Lec_036.cpp
@@ -0,0 +1,31 @@
+﻿// STL 4월 18일 강의기록
+
+// Deque - 중복제거
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <fstream>
+#include <algorithm>
+#include "String.h"
+
+using namespace std;
+
+int main()
+{
+    ifstream in{ "Lec_036.cpp" };
+    list<String> cont(istream_iterator<String>{in}, {});
+
+    vector<String> v{ cont.begin(), cont.end() };
+
+    // sort는 random access족만 해줌. 
+    cont.sort([](String& a, String& b) {
+        return a.getString() < b.getString();
+        });
+    cont.unique();  // sort하고 안쓰면 중복된거 안사라짐.
+
+    for (String& s : v)
+        cout << s << endl;
+
+}

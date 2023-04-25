40ac51f9dabb7f131c5e656d52d3bbfadc10377c
MiyeongEom <aldjd020420@naver.com>
2023-04-25 16:21:25 +0900

    배열 - 파일 출력해서 저장, 정렬

---
+++ Lec_022.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <fstream>
+#include <array>
+using namespace std;
+
+
+int main()
+{
+    // 파일 "소스.cpp"에서 단어 100개를 a로 읽어와라
+    array<std::string, 100> a;      // 숙제 std::string -> String 으로 바꿔서 돌아가도록 코딩
+
+    ifstream in{ "Lec_022.cpp" };
+
+    for (int i = 0; i < 100; ++i)
+        in >> a[i];
+
+    // 오름차순정렬
+    for (std::string& str : a)
+        cout << str << endl;
+}

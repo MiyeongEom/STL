0f7cc6a7e88c9417181fb9aa36c5729118fad94f
MiyeongEom <aldjd020420@naver.com>
2023-04-25 16:22:47 +0900

    vector -첫 주소 반환 &v[0]

---
+++ Lec_033.cpp
@@ -0,0 +1,22 @@
+﻿// STL 4월 13일 강의기록
+
+// vector
+// 첫 주소 반환 &v[0]
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include "String.h"
+using namespace std;
+
+//--------
+int main()
+//--------
+{
+    vector<int> v{ 1,2,3,4,5,6 };
+
+    if (v.data() == &v[0]) // 첫 주소 반환
+        cout << "이거 출력안되면 노트북 버려야" << endl;
+}
+
+
